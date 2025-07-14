1713c5fd38c9bc669ccff2f73ff3731a7ae8fea1
Amine <amine@post.com>
2025-07-14 11:50:36 +0200

    Clean up

--- one-platform/src/app/app.ts
+++ one-platform/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { routeAnimations } from 'shared/animations/route-animations';
 import { TranslationService } from 'shared/services/translation.service';
 import { CommonModule } from '@angular/common';
@@ -12,15 +12,11 @@ import { CommonModule } from '@angular/common';
   animations: routeAnimations,
 })
 export class App {
-  protected title = 'angular-workspace';
+
+  title = "OnePlatform";
   private translation = inject(TranslationService);
-  private router = inject(Router);
 
   constructor() {
     this.translation.setLanguage('de');
-  }
-
-  alert(msg: string) {
-    window.alert(msg);
   }
 }
