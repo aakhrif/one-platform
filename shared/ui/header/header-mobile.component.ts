59da9ef58ccd14337b47150365caf4952730b420
Amine <amine@post.com>
2025-07-20 20:04:53 +0200

    Improve responsive hero section

--- shared/ui/header/header-mobile.component.ts
+++ shared/ui/header/header-mobile.component.ts
@@ -4,11 +4,12 @@ import { DeviceService } from '../../services/device.service';
 import { TopBannerService } from '../../services/top-banner.service';
 import { SearchComponent } from '../../../features/search/search.component';
 import { NgClass } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ui-header-mobile',
   standalone: true,
-  imports: [SearchComponent, NgClass],
+  imports: [SearchComponent, NgClass, RouterModule],
   templateUrl: './header-mobile.component.html',
   styleUrls: ['./header-mobile.component.scss']
 })
