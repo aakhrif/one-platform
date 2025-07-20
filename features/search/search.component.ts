01a611b81071daf8b832c6045a601004e272bcab
Amine <amine@post.com>
2025-07-20 15:11:22 +0200

    Added signal-based query handling, Debounce and validation via RxJS, Results served efficiently with switchMap

--- features/search/search.component.ts
+++ features/search/search.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'ui-search',
@@ -12,6 +15,22 @@ import { NgClass } from '@angular/common';
 export class SearchComponent {
   query = signal('');
   focused = false;
+  searchService = inject(SearchService);
+
+  constructor() {
+    toObservable(this.query).pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter(this.isValidSearch),
+      switchMap(searchTerm => this.searchService.searchJobs(searchTerm))
+    ).subscribe(results => {
+      console.log('search results: ', results);
+    });
+  }
+
+  isValidSearch(query: string): boolean {
+    return query.length > 2 && /^[a-zA-Z0-9 ]+$/.test(query);
+  }
 
   onQueryChange(event: Event): void {
     this.query.set((event.target as HTMLInputElement).value);
--- features/search/search.service.ts
+++ features/search/search.service.ts
@@ -1,8 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+export interface JobModel {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+  type: string;
+  skills: string[];
+  description: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class Search {
-  
+export class SearchService {
+  private jobsUrl = 'assets/jobs.json';
+
+  constructor(private http: HttpClient) {}
+
+  searchJobs(query: string) {
+    return this.http.get<JobModel[]>(this.jobsUrl).pipe(
+      map(jobs => jobs.filter(job =>
+        job.title.toLowerCase().includes(query.toLowerCase()) ||
+        job.description.toLowerCase().includes(query.toLowerCase()) ||
+        job.skills.some((skill: string) => skill.toLowerCase().includes(query.toLowerCase()))
+      ))
+    );
+  }
 }
